b9ee99c32cfec13f3fae6c983ffca3f9191be210
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-12-08 15:04:40 -0700

    concert function works. Cannot accept multiple words

--- liri.js
+++ liri.js
@@ -1,3 +1,46 @@
-requestAnimationFrame("dotenv").config();
+require("dotenv").config();
 var keys=require("./keys.js");
-var spotify=new spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var keys = require("./keys.js");
+var multipleWordBand=[];
+// var spotify=new spotify(keys.spotify);
+// for (var i = 3; i< arg.length; i++) {
+//     reference.push(arg[i])
+// }
+// var referenceBand = reference.join("");
+console.log(process.argv.length)
+
+    for (var i = 2; i<process.argv.length; i++) {
+        j=(process.argv[i]);
+        multipleWordBand.push(j);
+        console.log(j);
+    }
+    console.log(multipleWordBand);
+    band = multipleWordBand.join("");
+    console.log(band);
+
+function concert() {
+    var concerts = "https://rest.bandsintown.com/artists/" +band+ "/events?app_id=codingbootcamp";
+    axios.get(concerts)
+    .then(
+        function (response) {
+        console.log("   ");
+        console.log("*****  Here are the dates and locations for " +band+"  *****");
+    for (var i = 0; i<response.data.length; i++) {
+        var datetime = response.data[i].datetime;
+        var dateArr = datetime.split("T");
+        var results = 
+        "\n-------------------------------------\n" +
+        "\nVenue Name: " + response.data[i].venue.name +
+        "\nVenue Location: " + response.data[i].venue.city +", "+ response.data[i].venue.region +
+        "\nDate of Event: " + moment(dateArr[0], "YYYY-MM-DD").format("MM/DD/YYYY");
+        console.log(results);
+        }
+        console.log("  ");
+        console.log("********************************");
+        console.log("  ");
+    }
+    )
+}
+concert();
