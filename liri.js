6cd7b36925fedcf0ab54adcd512f9d0754a6974c
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-12-09 23:10:50 -0700

    All operations work except spotify

--- liri.js
+++ liri.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 var keys=require("./keys.js");
+var spotify=require("node-spotify-api");
 var axios = require("axios");
 var moment = require("moment");
 var keys = require("./keys.js");
-var multipleWordInput=[];
 const fs = require('fs');
-// var spotify=new spotify(keys.spotify);
+input=process.argv.slice(3).join(" ");
 
 switch(process.argv[2]) {
-    case "concert-this":  concert(); break;
-    case "spotify-this-song": console.log("Spotify something here"); break;
-    case "movie-this": movies(); break;
-    case "do-what-it-says": console.log("Do something here"); break;
+    case "concert-this":  concert(input); break;
+    case "spotify-this-song": spotifySong(input); break;
+    case "movie-this": movies(input); break;
+    case "do-what-it-says": random(); break;
     default: console.log("Sorry, that expression isn't understood.");
 }
 
@@ -22,18 +22,13 @@ function output(i){
   });}
 
 // CONCERT-THIS
-function concert() {
-for (var i = 3; i<process.argv.length; i++) {
-        j=(process.argv[i]);
-        multipleWordInput.push(j);
-    }
-    band = multipleWordInput.join("%20"); 
-    var concertResults = "https://rest.bandsintown.com/artists/" +band+ "/events?app_id=codingbootcamp";
+function concert(input) {
+    var concertResults = "https://rest.bandsintown.com/artists/" +input+ "/events?app_id=codingbootcamp";
     axios.get(concertResults)
     .then(
         function (response) {
         console.log("   ");
-        console.log("*****  Here are the dates and locations for " +band+"  *****");
+        console.log("*****  Here are the dates and locations for " +input+"  *****");
     for (var i = 0; i<response.data.length; i++) {
         var datetime = response.data[i].datetime;
         var dateArr = datetime.split("T");
@@ -43,7 +38,7 @@ for (var i = 3; i<process.argv.length; i++) {
         "\nVenue Location: " + response.data[i].venue.city +", "+ response.data[i].venue.region +
         "\nDate of Event: " + moment(dateArr[0], "YYYY-MM-DD").format("MM/DD/YYYY");
         console.log(results);
-        var func="\n\n\n-------------------------------------\nCommand: concert-this\nBand Searched: "+response.data[i].artist.name
+        var func="\n\n\n-------------------------------------\nCommand: concert-this\nBand Searched: "+input
         output(func);
         output(results);
         }
@@ -55,19 +50,17 @@ for (var i = 3; i<process.argv.length; i++) {
 }
 
 // MOVIE-THIS
-function movies() {
-    for (var i = 3; i<process.argv.length; i++) {
-            j=(process.argv[i]);
-            multipleWordInput.push(j);
-        }
-        movie = multipleWordInput.join("%20"); 
-        var movieResults = "https://omdbapi.com/?apikey=7fb908b7&t=" + movie;
+function movies(input) {
+    if (!input) {
+        input = "Mr. Nobody";
+    }
+        var movieResults = "https://omdbapi.com/?apikey=7fb908b7&t=" + input;
         axios.get(movieResults)
     .then(
         function (response) {
         var movieData=(response.data);
         console.log("   ");
-        console.log("*****  Here is the data for " +movie+"  *****");
+        console.log("*****  Here is the data for " +input+"  *****");
         var results = 
         "\n-------------------------------------\n" +
         "\nTitle: " + movieData.Title +
@@ -89,9 +82,66 @@ function movies() {
         
     }
 
-
-
 //SPOTIFY-THIS
+function spotifySong (songName) {
+    var spotify=new spotify(keys.spotify);
+    // console.log(spotify);
+    if(!songName) {
+        songName = "Kiss Me";
+    };
+
+// for (var i = 3; i<process.argv.length; i++) {
+//     j=(process.argv[i]);
+//     multipleWordInput.push(j);
+// }
+// songName=multipleWordInput;
+// console.log("1");
+// console.log(songName);
+// console.log("2");
+//     if (songName === undefined) {
+//         songName = "What's my age again";
+//     }
+//     spotify.search(
+//         {
+//             type: "track",
+//             query: "kiss me"
+//         },
+//         function (err, data) {
+//             if (err) {
+//                 console.log("Error occurred: " + err);
+//                 return;
+//             }
+
+//             var songs = data.tracks.items;
+
+//             for (var i = 0; i < songs.length; i++) {
+//                 console.log(i);
+//                 console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+//                 console.log("song name: " + songs[i].name);
+//                 console.log("preview song: " + songs[i].preview_url);
+//                 console.log("album: " + songs[i].album.name);
+//                 console.log("-----------------------------------");
+//             }
+//         }
+//     );
+};
  
 //DO-WHAT-IT-SAYS
-
+function random() {
+fs.readFile("random.txt", "utf8", function (error, data) {
+    if(error) {
+        return console.log(error);
+    }else{
+        var random=data.split(",");
+        var input =(random[0]);
+        var j = random[1];
+        switch(input) {
+            case "concert-this":  concert(j); break;
+            case "spotify-this-song": spotifySong(j); break;
+            case "movie-this": movies(j); break;
+            case "do-what-it-says": random(); break;
+            default: console.log("Sorry, that expression isn't understood.");
+        }
+    }
+})
+}
